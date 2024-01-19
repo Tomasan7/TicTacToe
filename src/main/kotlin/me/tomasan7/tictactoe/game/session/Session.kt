02e14566336236449105997a56073795cebd1e28
Tomasan7 <tomashula06@gmail.com>
2024-01-19 13:29:16 +0100

    Remove unused files

--- src/main/kotlin/me/tomasan7/tictactoe/game/session/Session.kt
+++
@@ -1,9 +0,0 @@
-package me.tomasan7.tictactoe.game.session
-
-import me.tomasan7.tictactoe.game.packet.ClientPacket
-
-interface Session
-{
-    suspend fun sendPacket(packet: ClientPacket)
-    suspend fun close(message: String = "")
-}
