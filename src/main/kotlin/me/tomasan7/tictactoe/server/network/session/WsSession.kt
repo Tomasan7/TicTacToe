c98402ebdcafc6c6d3c82ee6b95a377dc20973fe
Tomasan7 <tomashula06@gmail.com>
2024-01-19 13:29:01 +0100

    Create websocket implementation of Session

---
+++ src/main/kotlin/me/tomasan7/tictactoe/server/network/session/WsSession.kt
@@ -0,0 +1,71 @@
+package me.tomasan7.tictactoe.server.network.session
+
+import io.ktor.server.plugins.*
+import io.ktor.server.websocket.*
+import io.ktor.websocket.*
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ReceiveChannel
+import me.tomasan7.tictactoe.server.network.packet.InvalidPacketFormatException
+import me.tomasan7.tictactoe.server.network.packet.InvalidPacketIdException
+import me.tomasan7.tictactoe.server.network.packet.client.ClientPacket
+import me.tomasan7.tictactoe.server.network.packet.client.JsonClientPacketDeserializer
+import me.tomasan7.tictactoe.server.network.packet.server.ServerPacket
+import me.tomasan7.tictactoe.server.network.packet.server.ServerPacketSerializer
+import org.slf4j.LoggerFactory
+
+class WsSession(
+    private val wsSession: WebSocketServerSession,
+    private val serverPacketSerializer: ServerPacketSerializer,
+    private val clientPacketDeserializer: JsonClientPacketDeserializer
+) : Session
+{
+    private val logger = LoggerFactory.getLogger(WsSession::class.java)
+    private val remoteHost = wsSession.call.request.origin.remoteHost
+
+    private val _incomingPacketsChannel = Channel<ClientPacket>()
+    override val incomingPacketsChannel: ReceiveChannel<ClientPacket> = _incomingPacketsChannel
+
+    /**
+     * Intended to only be called by the websockets handler.
+     */
+    suspend fun receiveFrame(incomingFrame: Frame)
+    {
+        if (incomingFrame is Frame.Text)
+        {
+            val frameText = incomingFrame.readText()
+            val packet = try
+            {
+                clientPacketDeserializer.deserializePacket(frameText)
+            }
+            catch (e: InvalidPacketFormatException)
+            {
+                logger.warn("Received an invalid packet format from $remoteHost: \n'$frameText'")
+                return
+            }
+            catch (e: InvalidPacketIdException)
+            {
+                logger.warn("Received a packet with an invalid id from $remoteHost: \n'$frameText'")
+                return
+            }
+
+            _incomingPacketsChannel.send(packet)
+            logger.debug("Received a packet from $remoteHost: \n'$frameText'")
+        }
+        else
+            logger.warn("Received non text frame of type ${incomingFrame.frameType} from $remoteHost")
+    }
+
+    override suspend fun sendPacket(packet: ServerPacket)
+    {
+        val serializedPacket = serverPacketSerializer.serializePacket(packet)
+        val frame = Frame.Text(serializedPacket)
+        wsSession.send(frame)
+
+        logger.debug("Sending packet to $remoteHost: \n'$serializedPacket'")
+    }
+
+    override suspend fun close(message: String)
+    {
+        wsSession.close(CloseReason(CloseReason.Codes.NORMAL, message))
+    }
+}
