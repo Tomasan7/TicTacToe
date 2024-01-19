150a24d3bc33f83f8daa1f433c75a9d22540fd2d
Tomasan7 <tomashula06@gmail.com>
2024-01-19 21:43:50 +0100

    Remove unused classes

--- src/main/kotlin/me/tomasan7/tictactoe/game/join/JoinState.kt
+++
@@ -1,8 +0,0 @@
-package me.tomasan7.tictactoe.game.join
-
-sealed interface JoinState
-{
-    data object SelectName : JoinState
-    data object SelectSymbol: JoinState
-    data object SelectColor: JoinState
-}
