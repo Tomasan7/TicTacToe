edbe8c6e296202a5a646ca0f7c0b70d959634408
Tomasan7 <tomashula06@gmail.com>
2024-01-26 19:57:22 +0100

    Frontend WIP

--- src/jvmMain/kotlin/me/tomasan7/tttweb/plugins/Routing.kt
+++ src/jvmMain/kotlin/me/tomasan7/tttweb/plugins/Routing.kt
@@ -1,6 +1,7 @@
 package me.tomasan7.tttweb.plugins
 
 import io.ktor.server.application.*
+import io.ktor.server.freemarker.*
 import io.ktor.server.http.content.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -9,5 +10,10 @@ fun Application.configureRouting()
 {
     routing {
         staticResources("/static/", "/pages/")
+        staticResources("/frontend/", "/frontend/")
+
+        get("/") {
+            call.respondTemplate("index.ftlh")
+        }
     }
 }
