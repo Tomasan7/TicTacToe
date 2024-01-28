7301cb8de3277110a763d6d2d2cd8cc1d8e72835
Tomasan7 <tomashula06@gmail.com>
2024-01-28 14:20:19 +0100

    Fix Color.valueHex not working properly

--- src/commonMain/kotlin/me/tomasan7/tictactoe/util/Color.kt
+++ src/commonMain/kotlin/me/tomasan7/tictactoe/util/Color.kt
@@ -6,5 +6,5 @@ class Color(val value: Int)
     constructor(red: UByte, green: UByte, blue: UByte):
             this((red.toInt() and 0xFF shl 16) or (green.toInt() and 0xFF shl 8) or (blue.toInt() and 0xFF))
 
-    val valueHex = value.toString(16).removePrefix("00")
+    val valueHex = value.toString(16).padStart(6, '0')
 }
